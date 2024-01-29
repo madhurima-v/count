43296743429ddc97bc22cd5bb1ec9992cd7d3971
Madhurima <madhurimavispute61@gmail.com>
2024-01-29 20:11:46 +0530

    animations

---
+++ zoom.js
@@ -0,0 +1,25 @@
+document.getElementsByClassName("img")[0].onmouseover = function(){
+        document.getElementsByClassName("img")[0].classList.add("zoom")
+    }
+    
+    document.getElementsByClassName("img")[0].onmouseleave = function(){
+        document.getElementsByClassName("img")[0].classList.remove("zoom")
+    }
+    
+    document.getElementsByClassName("img1")[0].onmouseover = function(){
+        document.getElementsByClassName("img1")[0].classList.add("zoom1")
+    }
+    
+    document.getElementsByClassName("img1")[0].onmouseleave = function(){
+        document.getElementsByClassName("img1")[0].classList.remove("zoom1")
+    }
+    
+    document.getElementsByClassName("img2")[0].onmouseover = function(){
+        document.getElementsByClassName("img2")[0].classList.add("zoom2")
+    }
+    
+    document.getElementsByClassName("img2")[0].onmouseleave = function(){
+        document.getElementsByClassName("img2")[0].classList.remove("zoom2")
+    }
+    
+    
